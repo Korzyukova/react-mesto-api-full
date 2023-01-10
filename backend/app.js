448c20e9d031765514d91fdc1b9ca9c870e518ab
Tatiana Korzyukova <korzioukova@gmail.com>
2023-01-09 22:37:01 -0500

    headers

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,23 @@ app.use((req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', '*');
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    }
+
+    const requestHeaders = req.headers['access-control-request-headers'];
+    if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // завершаем обработку запроса и возвращаем результат клиенту
+      return res.end();
+    }
   }
 
   next();
