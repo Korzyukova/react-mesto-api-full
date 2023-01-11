a6db30711f60f468fdc7e41a6877e0b7eabc8264
Tatiana Korzyukova <korzioukova@gmail.com>
2023-01-10 23:15:27 -0500

    add likes

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -49,6 +49,7 @@ module.exports.createCard = (req, res, next) => {
 
 module.exports.likeCard = (req, res, next) => {
   const { cardId } = req.params;
+  console.log(cardId)
   Card.findByIdAndUpdate(
     cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
@@ -66,6 +67,7 @@ module.exports.likeCard = (req, res, next) => {
 
 module.exports.dislikeCard = (req, res, next) => {
   const { cardId } = req.params;
+  console.log(cardId)
   Card.findByIdAndUpdate(
     cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
